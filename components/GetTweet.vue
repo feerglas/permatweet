73f2f014254d9596fb8095914b73ae5c5d5a7faf
Yves Tscherry <info@feerglas.com>
2022-03-12 12:18:55 +0100

    add logic to get tweet from lambda function

--- components/GetTweet.vue
+++ components/GetTweet.vue
@@ -1,13 +1,17 @@
 <template>
-  <div >
+  <div>
     <v-text-field
-      label="Main input"
-      hide-details="auto"
+      v-model="input"
+      label="Tweet ID"
+      placeholder="asdf"
+      @input.native="handleInputChange"
     />
     <v-btn
       elevation="2"
       large
       x-large
+      :disabled="!inputValid"
+      @click="getTweet"
     >
       Get tweet
     </v-btn>
@@ -16,6 +20,32 @@
 
 <script>
 export default {
-  name: 'GetTweet'
+  name: 'GetTweet',
+  data () {
+    return {
+      input: '1502533647336972290',
+      inputValid: false
+    }
+  },
+  methods: {
+    async getTweet () {
+      const url = `./.netlify/functions/twitter?id=${this.input}`
+
+      try {
+        const data = await fetch(url)
+        const response = await data.json()
+
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    handleInputChange () {
+      this.inputValid = this.input.length > 0
+    }
+  }
 }
 </script>
+
+<style>
+</style>
