cbd5927eab195f4163f3a77805da1bc096044080
Yves Tscherry <info@feerglas.com>
2022-03-12 20:01:55 +0100

    add hint about browser extension

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,8 @@
       </v-card>
       <GetTweet />
       <StoreArweave />
+
+      <p>Keep in mind: you need the ArConnect Browser Extension installed for this App to work. You can get it here: <a href="https://www.arconnect.io/" rel="noopener noreferrer" target="_blank">https://www.arconnect.io/</a></p>
     </v-col>
   </v-row>
 </template>
