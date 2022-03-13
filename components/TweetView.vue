1d1f8445c621a045dca758d2663456f6fe153515
Yves Tscherry <info@feerglas.com>
2022-03-13 20:32:01 +0100

    add component for tweet preview and tweet markup

---
+++ components/TweetView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>
+      here we will place a preview of the tweet. same markup will be used to store the tweet into arweave.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TweetView'
+}
+</script>
+
+<style>
+</style>
