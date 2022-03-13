546fa74cb9c6289c1325876bf11864aa6462102a
Yves Tscherry <info@feerglas.com>
2022-03-13 21:38:16 +0100

    add helper file to query all transactions

---
+++ web3/queries.js
@@ -0,0 +1,17 @@
+import Arweave from 'arweave'
+import ArDB from 'ardb'
+import config from './config'
+
+const arweave = Arweave.init()
+const ardb = new ArDB(arweave)
+
+export const getAllTransactions = async () => {
+  try {
+    const transactions = await ardb.search('transactions').appName(config.transaction.tags['App-Name']).findAll()
+
+    return transactions
+  } catch (err) {
+    console.error('graph-ql failed in getAllTransactions')
+    return false
+  }
+}
