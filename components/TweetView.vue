2aee585af78925b3a2d854f6cc4e645ca884c2fa
Yves Tscherry <info@feerglas.com>
2022-03-13 20:37:44 +0100

    render content pased  on state  of tweetContent

--- components/TweetView.vue
+++ components/TweetView.vue
@@ -3,12 +3,21 @@
     <p>
       here we will place a preview of the tweet. same markup will be used to store the tweet into arweave.
     </p>
+
+    <p v-if="tweetContent">
+      tweet content received. The received json is now available under the variable "tweetContent"
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TweetView'
+  name: 'TweetView',
+  computed: {
+    tweetContent () {
+      return this.$store.state.twitter.tweetContent
+    }
+  }
 }
 </script>
 
