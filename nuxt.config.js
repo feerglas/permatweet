810d76214cb2813f4ea2dff35062488c1559314d
Yves Tscherry <info@feerglas.com>
2022-03-13 18:09:06 +0100

    revert to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  target: 'server',
+  target: 'static',
   head: {
     titleTemplate: '%s - permatweet',
     title: 'permatweet',
