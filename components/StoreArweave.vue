2a8ae082b5e8a8a7a47c28fbdaf9da2ab48278da
Yves Tscherry <info@feerglas.com>
2022-03-13 16:17:57 +0100

    disable while confirming

--- components/StoreArweave.vue
+++ components/StoreArweave.vue
@@ -4,7 +4,7 @@
       elevation="2"
       large
       x-large
-      :disabled="!tweetContent || storing"
+      :disabled="!tweetContent || storing || confirming"
       @click="store"
     >
       Store tweet
@@ -26,6 +26,9 @@ import { storeOnArweave } from '../web3/arweave'
 export default {
   name: 'StoreArweave',
   computed: {
+    confirming () {
+      return this.$store.state.arweave.confirming
+    },
     error () {
       return this.$store.state.arweave.error
     },
@@ -37,6 +40,9 @@ export default {
     },
     tweetContent () {
       return this.$store.state.twitter.tweetContent
+    },
+    tweetId () {
+      return this.$store.state.twitter.tweetId
     }
   },
   methods: {
@@ -45,7 +51,7 @@ export default {
         this.$store.commit('arweave/storing', true)
         this.$store.commit('arweave/error', false)
         this.$store.commit('arweave/id', false)
-        const trxId = await storeOnArweave(this.tweetContent)
+        const trxId = await storeOnArweave(this.tweetContent, this.tweetId)
         this.$store.commit('arweave/storing', false)
         this.$store.commit('arweave/id', trxId)
       } catch (e) {
