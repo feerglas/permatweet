016404958aad64bb881e5e94f95a5b295ff47e77
Yves Tscherry <info@feerglas.com>
2022-03-13 20:31:50 +0100

    add  sample query to test graph-ql

--- components/SavedTweetsCount.vue
+++ components/SavedTweetsCount.vue
@@ -5,43 +5,23 @@
 </template>
 
 <script>
-// import Arweave from 'arweave'
-// const arweave = Arweave.init()
+import Arweave from 'arweave'
+import { queryAllSavedTweets } from '../web3/queries'
 
-// const queryObject = {
-//   query: `
-// {
-//   transactions(tags: [{
-//     name: "permaweb-intend",
-//     values: ["saved-tweet"]
-//   }])
-//   {
-//     edges {
-//       node {
-//         id
-//         tags {
-//           name
-//           value
-//         }
-//       }
-//     }
-//   }
-// }
-// `
-// }
+const arweave = Arweave.init()
 
 export default {
-  name: 'SavedTweetsCount'
-  // async created () {
-  //   try {
-  //     const results = await arweave.api.post('/graphql', queryObject)
-  //     const edges = results.data.data.transactions.edges
-  //     console.log(edges)
-  //   } catch (err) {
-  //     console.error('GraphQL query failed')
-  //     throw new Error(err)
-  //   }
-  // }
+  name: 'SavedTweetsCount',
+  async mounted () {
+    try {
+      const results = await arweave.api.post('/graphql', queryAllSavedTweets)
+      const edges = results.data.data.transactions.edges
+      console.log(edges)
+    } catch (err) {
+      console.error('GraphQL query failed')
+      throw new Error(err)
+    }
+  }
 }
 
 </script>
