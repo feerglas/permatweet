736724e1d334db40379ef5b4fdeb951599fc3925
Yves Tscherry <info@feerglas.com>
2022-03-13 20:59:18 +0100

    try out ardb

--- components/SavedTweetsCount.vue
+++ components/SavedTweetsCount.vue
@@ -6,17 +6,21 @@
 
 <script>
 import Arweave from 'arweave'
-import { queryAllSavedTweets } from '../web3/queries'
+import ArDB from 'ardb'
 
 const arweave = Arweave.init()
+const ardb = new ArDB(arweave)
 
 export default {
   name: 'SavedTweetsCount',
   async mounted () {
     try {
-      const results = await arweave.api.post('/graphql', queryAllSavedTweets)
-      const edges = results.data.data.transactions.edges
-      console.log(edges)
+      const tx = await ardb.search('transaction').id('wdvU5aHM1bLjPuygO9UmS8GOeXHae6dPPStJqyZ0DIA').findOne()
+
+      const appName = await ardb.search('transactions').appName('SmartWeaveAction').findOne()
+
+      console.log(tx)
+      console.log(appName)
     } catch (err) {
       console.error('GraphQL query failed')
       throw new Error(err)
