1247c227c4b362540815f7e014cbd971b76e19f1
Yves Tscherry <info@feerglas.com>
2022-03-14 12:35:24 +0100

    add properties for component content and tweet data

--- store/twitter.js
+++ store/twitter.js
@@ -2,6 +2,8 @@ export const state = () => ({
   fetchError: false,
   fetching: false,
   tweetContentDocument: false,
+  tweetContentComponent: false,
+  tweetData: false,
   tweetId: false,
   tweetAlreadySaved: false
 })
@@ -15,6 +17,12 @@ export const mutations = {
   },
   tweetContentDocument (state, content) {
     state.tweetContentDocument = content
+  },
+  tweetContentComponent (state, content) {
+    state.tweetContentComponent = content
+  },
+  tweetData (state, content) {
+    state.tweetData = content
   },
   tweetId (state, id) {
     state.tweetId = id
