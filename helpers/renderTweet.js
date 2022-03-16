6ec282893bc9b7b55844f4a137aaf4749f753e40
Yves Tscherry <info@feerglas.com>
2022-03-16 23:33:01 +0100

    anonymise data

--- helpers/renderTweet.js
+++ helpers/renderTweet.js
@@ -2,6 +2,24 @@ import { formatDate } from './date'
 import { ellipsisMiddle } from './ellipsis'
 
 export default (data, isForDocument) => {
+  const today = formatDate(new Date())
+
+  // anonymise tweet data
+  data.data.author_id = ellipsisMiddle(data.data.author_id)
+  delete data.data.entities
+
+  // anonymise user data
+  data.includes.users.forEach((user) => {
+    user.id = ellipsisMiddle(user.id)
+    user.username = ellipsisMiddle(user.username)
+    user.name = ellipsisMiddle(user.name)
+    user.description = ellipsisMiddle(user.description)
+    delete user.profile_image_url
+    delete user.entities
+  })
+
+  console.log(data)
+
   const prepend = `
 <!doctype html>
 <html>
@@ -24,10 +42,10 @@ export default (data, isForDocument) => {
       ${data.data.text}
     </h1>
     <div class='permatweet__meta' style='font-size: 0.85rem; color: #000;'>
-    <span>Tweet author: ${ellipsisMiddle(data.data.author_id)}</span> <span>aka</span> <span>${ellipsisMiddle(data.includes.users[0].username)}</span>
+    <span>Tweet author: ${data.data.author_id}</span> <span>aka</span> <span>${data.includes.users[0].username}</span>
     <span>
       <span> | Tweet created: <time datetime="${formatDate(new Date(data.data.created_at))}">${formatDate(new Date(data.data.created_at))}</time></span>
-      <span> | Tweet permanently saved: <time datetime="${formatDate(new Date())}">${formatDate(new Date())}</time></span>
+      <span> | Tweet permanently saved: <time datetime="${today}">${today}</time></span>
     </span>
   </div>
   </article>
