9c71a1510b15346f01e0134cfaedefb298456dda
Yves Tscherry <info@feerglas.com>
2022-03-17 00:06:04 +0100

    globally store og properties

---
+++ helpers/ogProperties.js
@@ -0,0 +1,8 @@
+export default [
+  { property: 'og:title', content: 'permatweet' },
+  { property: 'og:image', content: 'http://arweave.net/PwLnzYGJqtBpJA68xqVGBHb_CzVGTVht4vmMHPo001A' },
+  { property: 'og:url', content: 'https://permatweet.netlify.app/' },
+  { property: 'og:type', content: 'website' },
+  { property: 'og:description', content: 'Store tweets on the Arweave Blockchain on demand - forever...' },
+  { property: 'og:locale', content: 'en_US' }
+]
