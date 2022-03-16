30ce1546e7c876edc0978f84db54260148cac3be
Yves Tscherry <info@feerglas.com>
2022-03-16 22:34:21 +0100

    move footer contents to own component

---
+++ components/PageFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-footer
+    :absolute="true"
+    app
+    class="d-flex justify-space-between"
+    color="blue-grey lighten-5"
+  >
+    <span class="mr-2">&copy; {{ new Date().getFullYear() }}</span>
+
+    <SavedTweetsCount class="my-2" />
+
+    <ButtonLink
+      link="https://github.com/feerglas/permatweet"
+      text="GitHub"
+      icon="mdi-github"
+      :outlined="true"
+      :small="true"
+      class="ml-2"
+    />
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'PageFooter'
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,25 +5,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="true"
-      app
-      class="d-flex justify-space-between"
-      color="blue-grey lighten-5"
-    >
-      <span class="mr-2">&copy; {{ new Date().getFullYear() }}</span>
-
-      <SavedTweetsCount class="my-2" />
-
-      <ButtonLink
-        link="https://github.com/feerglas/permatweet"
-        text="GitHub"
-        icon="mdi-github"
-        :outlined="true"
-        :small="true"
-        class="ml-2"
-      />
-    </v-footer>
+    <PageFooter />
   </v-app>
 </template>
 
