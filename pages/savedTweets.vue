db611bfb97cb3d8515b6bc309ef57277310e6613
Yves Tscherry <info@feerglas.com>
2022-03-18 22:19:49 +0100

    add empty page for savedTweets

---
+++ pages/savedTweets.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ transactions.map((transaction) => {
+      return `trx: ${transaction.id}`
+    }) }}
+  </div>
+</template>
+
+<script>
+import { getAllTransactions } from '../web3/queries'
+
+export default {
+  name: 'SavedTweets',
+  data () {
+    return {
+      transactions: []
+    }
+  },
+  async mounted () {
+    const transactions = await getAllTransactions()
+
+    console.log(transactions)
+  }
+}
+</script>
