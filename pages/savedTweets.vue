db092fa0ef02190ef2c55830e6ec5f6816671d53
Yves Tscherry <info@feerglas.com>
2022-03-18 23:20:56 +0100

    add table with data

--- pages/savedTweets.vue
+++ pages/savedTweets.vue
@@ -1,25 +1,74 @@
 <template>
-  <div>
-    {{ transactions.map((transaction) => {
-      return `trx: ${transaction.id}`
-    }) }}
-  </div>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="50"
+    class="elevation-2"
+  />
 </template>
 
 <script>
 import { getAllTransactions } from '../web3/queries'
+import { transactionTagKeys } from '../web3/config'
 
 export default {
   name: 'SavedTweets',
   data () {
     return {
-      transactions: []
+      headers: [],
+      items: []
     }
   },
-  async mounted () {
-    const transactions = await getAllTransactions()
+  async created () {
+    const rawData = await getAllTransactions()
 
-    console.log(transactions)
+    this.headers = [
+      {
+        text: 'Transaction ID',
+        value: 'transactionId'
+      },
+      {
+        text: 'Transaction Owner',
+        value: 'transactionOwner'
+      },
+      {
+        text: 'Tweet Id',
+        value: 'tweetId'
+      },
+      {
+        text: 'Tweet created date',
+        value: 'tweetCreatedDate'
+      },
+      {
+        text: 'Tweet saved date',
+        value: 'tweetSavedDate'
+      },
+      {
+        text: 'Tweet author id',
+        value: 'tweetAuthorId'
+      },
+      {
+        text: 'Tweet author username',
+        value: 'tweetAuthorUsername'
+      },
+      {
+        text: 'Tweet preview',
+        value: 'tweetPreview'
+      }
+    ]
+
+    this.items = rawData.map((item) => {
+      return {
+        transactionId: item._id,
+        transactionOwner: item._owner.address,
+        tweetId: item.tags[transactionTagKeys.tweetId],
+        tweetCreatedDate: item.tags[transactionTagKeys.tweetCreatedDate],
+        tweetSavedDate: item.tags[transactionTagKeys.tweetSavedDate],
+        tweetAuthorId: item.tags[transactionTagKeys.tweetAuthorId],
+        tweetAuthorUsername: item.tags[transactionTagKeys.tweetAuthorName],
+        tweetPreview: item.tags[transactionTagKeys.tweetContentPreview]
+      }
+    })
   }
 }
 </script>
