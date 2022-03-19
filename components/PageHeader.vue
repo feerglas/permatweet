83d278722bc53113f1fc363a4b387fa5d89431fb
Yves Tscherry <info@feerglas.com>
2022-03-19 11:35:35 +0100

    add top margin and divider

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
+  <div class="mt-6">
     <h1 class="text-h3">
       Permatweet
     </h1>
     <p class="lead">
       Store tweets on the Arweave Blockchain on demand - forever...
     </p>
+    <v-divider class="my-12" />
   </div>
 </template>
 
