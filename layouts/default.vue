f213c4ab01762f8a023ba9469dbebca0bd02051e
Yves Tscherry <info@feerglas.com>
2022-03-19 11:35:25 +0100

    add page header

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,11 @@
   <v-app dark>
     <v-main>
       <v-container>
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="8" md="8" lg="6">
+            <PageHeader />
+          </v-col>
+        </v-row>
         <Nuxt />
       </v-container>
     </v-main>
