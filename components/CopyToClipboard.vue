fe28b68b1f97ec166edfa8dbbc854c5c84edec69
Yves Tscherry <info@feerglas.com>
2022-03-20 18:15:12 +0100

    add component for copying text

---
+++ components/CopyToClipboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn
+    v-clipboard:copy="textToCopy"
+    rounded
+    color="primary"
+    dark
+    small
+    outlined
+  >
+    <v-icon
+      left
+      dark
+    >
+      mdi-content-copy
+    </v-icon>
+    {{ text }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'CopyToClipboard',
+  props: {
+    text: {
+      required: true,
+      type: String
+    },
+    textToCopy: {
+      required: true,
+      type: String
+    }
+  }
+}
+
+</script>
